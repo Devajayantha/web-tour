6d7a663493f27927702f8c7dae461567a010a21f
deva jayantha <devajayantha870@gmail.com>
2018-03-29 02:16:18 +0800

    update contact

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="{{asset('img/logo.png')}}">
     <link rel="stylesheet" href="{{asset('css/footer.css') }}">
     <link rel="stylesheet" href="{{asset('css/contact.css') }}">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="{{asset('css/fontawesome-all.css')}}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
