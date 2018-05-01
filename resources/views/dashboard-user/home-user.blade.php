7647efbe5a531c5e124e1af3ac9393e00e0e8564
deva jayantha <devajayantha870@gmail.com>
2018-05-01 13:50:18 +0800

    add

--- resources/views/dashboard-user/home-user.blade.php
+++ resources/views/dashboard-user/home-user.blade.php
@@ -60,12 +60,15 @@
                                     <!-- Modal content-->
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <h5 class="modal-title" id="exampleModalLabel">Upload Proof Payment</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                             </button>
                                           </div>
                                       <div class="modal-body">
+                                        <div class="alert alert-info" role="alert">
+                                          File Upload Maximum 5 mb
+                                        </div>
                                         {{--  Form upload pembyaran  --}}
                                         <form method='post' action='{{route("uploadbayar.update", ['uploadbayar'=>$b->id])}}' enctype="multipart/form-data">
                                             @method('patch')
